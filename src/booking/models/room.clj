04786e3ab637e8aa920114086bf1bb8b8745b53a
owain lewis <owain@owainlewis.com>
2013-02-27 13:27:25 +0000

    sketching out the room model :

--- src/booking/models/room.clj
+++ src/booking/models/room.clj
@@ -1,1 +1,20 @@
-(ns booking.models.room)
+(ns booking.models.room
+  (refer-clojure :exclude [create get])
+  (:use [booking.db :as db]))
+
+(defn all
+  "Returns all meeting rooms"
+  []
+  (db/get-all :rooms))
+
+(defn create 
+  "Create a new meeting room record"
+  [room]
+  (db/create-one :rooms room))
+
+(defn get 
+  "Find a room by id"
+  [id]
+  (db/get-one :rooms id))
+  
+  
